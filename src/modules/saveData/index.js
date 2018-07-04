64f0f873be72545a6d9944eb015bdfe7660bde5a
Hugo Larrousse <hugo.larrousse@gmail.com>
2018-07-04 09:02:45 +0200

    create architecture v1 for the service

---
+++ src/modules/saveData/index.js
@@ -0,0 +1,27 @@
+const mongo = require('../../db/mongo');
+const logger = require('../../utils');
+
+const { databaseSalesforce } = process.env;
+
+const collectionName = {
+  opportunities: 'opportunities',
+  events: 'events',
+};
+
+const saveData = async (dataType, documents) => {
+  const collection = collectionName[dataType] || null;
+  for (const doc of documents) {
+    // const filter = {
+    //   id: Number(doc.id),
+    //   team: Number(doc.team),
+    // };
+    try {
+      const result = await mongo.updateOne(databaseSalesforce, collection, filter, doc, { upsert: true });
+      console.log('result :', result);
+    } catch (e) {
+      logger.error(__filename, saveData.name, e.message);
+    }
+  }
+};
+
+exports.saveData = saveData;
