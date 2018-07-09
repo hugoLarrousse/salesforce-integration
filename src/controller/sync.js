ce293e0021f2d105fe425b9df3e4969961e8cf98
Hugo Larrousse <hugo.larrousse@gmail.com>
2018-07-09 17:51:53 +0200

    more data needed from h7

--- src/controller/sync.js
+++ src/controller/sync.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = require('../services/request');
 const check = require('../utils/check');
+const syncData = require('../services/syncData');
 
 const router = express.Router();
 
@@ -9,7 +10,6 @@ const TEMP_INSTANCE_URL = 'https://eu10.salesforce.com/';
 const TEMP_ROUTE = 'services/data/v43.0/query/';
 
 router.get('/events', async (req, res) => {
-  console.log('in');
   const query = 'SELECT+id,Subject +from+Event';
   try {
     const result = await request.salesforce(TEMP_INSTANCE_URL, TEMP_ROUTE, query, 'GET', {
@@ -35,9 +35,10 @@ router.get('/opportunities', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { integrationInfo } = req.body
+  const { integrationInfo, user, allIntegrations } = req.body;
   try {
     check.integrationInfo(integrationInfo);
+    const result = await syncData.everything(integrationInfo, user, allIntegrations);
     res.status(200).json(result);
   } catch (e) {
     console.log('e.message :', e.message);
