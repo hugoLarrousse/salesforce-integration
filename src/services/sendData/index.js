80a76f6689a23e2040b7cad9e1bf41355807d215
Hugo Larrousse <hugo.larrousse@gmail.com>
2018-07-09 17:51:29 +0200

    request to h7 for echoes

--- src/services/sendData/index.js
+++ src/services/sendData/index.js
@@ -1,1 +1,10 @@
-// to do
+const config = require('config');
+const request = require('../request');
+
+const H7_URL = config.get('h7Url');
+const { fixedToken } = process.env;
+
+module.exports = (data) => {
+  return request.salesforce(H7_URL, 'crm/echoes', null, 'POST', { Authorization: fixedToken }, data);
+};
+
