b73e136568ca749a3eefbe1c07e3a3b48eab54a9
Hugo Larrousse <hugo.larrousse@gmail.com>
2018-07-13 17:26:21 +0200

    log everywhere

--- src/services/syncData/index.js
+++ src/services/syncData/index.js
@@ -7,12 +7,17 @@ const dataTypeFOrEchoes = ['opportunity', 'task', 'event'];
 
 
 const syncByType = async (integrationInfo, dataType, user, allIntegrations, special) => {
+  console.log('IIII 2:');
   const results = await api.getData(integrationInfo.instanceUrl, integrationInfo.token, special || dataType);
+  console.log('IIII 3:');
   if (results && results.records) {
+    console.log('IIII 4:');
     const dataForEchoes = await saveData(dataType, results.records);
     if (dataTypeFOrEchoes.includes(dataType)) {
+      console.log('IIII 5:');
       const formattedData = await formatData.echoesInfo(dataForEchoes, dataType, user, allIntegrations);
       if (formattedData.toInsert.length > 0 || formattedData.toUpdate.length > 0 || (formatData.toUpsert && formatData.toUpsert.length > 0)) {
+        console.log('IIII 6:');
         await sendData.echoes(formattedData);
       }
     }
@@ -20,6 +25,7 @@ const syncByType = async (integrationInfo, dataType, user, allIntegrations, spec
 };
 
 exports.everything = async (integrationInfo, user, allIntegrations) => {
+  console.log('IIII 2:');
   await syncByType(integrationInfo, 'account');
   await Promise.all(['opportunity', 'task', 'event'].map(type => syncByType(integrationInfo, type, user, allIntegrations)));
   sendData.integration({ integration: { _id: integrationInfo._id, tokenExpiresAt: Date.now() + 86400000 } });
