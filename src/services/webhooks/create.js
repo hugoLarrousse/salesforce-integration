84753024a3faf1edc6a2dd83da2e2042297fcd7e
Hugo Larrousse <hugo.larrousse@gmail.com>
2018-07-16 17:17:04 +0200

    logs apex

--- src/services/webhooks/create.js
+++ src/services/webhooks/create.js
@@ -7,6 +7,7 @@ const triggersName = config.get('triggerName');
 exports.remoteProxy = async (organisationInfo) => {
   // Remote Site Settings
   const result = await api.postRemoteProxy(organisationInfo.instance_url, organisationInfo.access_token, template.h7RemoteSiteSettings);
+  console.log('resultREMOTEPROXY :', result);
   if (!result.success && result.errorCode !== 'DUPLICATE_DEVELOPER_NAME') {
     throw new Error(result.errorCode);
   }
@@ -15,6 +16,7 @@ exports.remoteProxy = async (organisationInfo) => {
 
 exports.apexClass = async (organisationInfo) => {
   const result = await api.postApexClass(organisationInfo.instance_url, organisationInfo.access_token, template.h7WebhookClass);
+  console.log('resultapexClass :', result);
   if (!result.success) {
     throw new Error(result.errorCode);
   }
@@ -23,6 +25,7 @@ exports.apexClass = async (organisationInfo) => {
 exports.apexTrigger = async (organisationInfo) => {
   for (const trigger of triggersName) {
     const result = await api.postApexTrigger(organisationInfo.instance_url, organisationInfo.access_token, template[trigger]);
+    console.log('resultapexTrigger :', result);
     if (!result.success) {
       throw new Error(result.errorCode);
     }
