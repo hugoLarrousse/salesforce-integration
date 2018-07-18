1a8cb71f730839a12b7537164d9571e7e03da6f5
Hugo Larrousse <hugo.larrousse@gmail.com>
2018-07-18 15:35:27 +0200

    remove useless routes

--- src/controller/sync.js
+++ src/controller/sync.js
@@ -1,41 +1,11 @@
 const express = require('express');
-const request = require('../services/request');
 const check = require('../utils/check');
 const syncData = require('../services/syncData');
 const middleware = require('../utils/middleware');
 
 const router = express.Router();
 
-const TEMP_BEARER = process.env.tempBearer;
-const TEMP_INSTANCE_URL = 'https://eu10.salesforce.com/';
-const TEMP_ROUTE = 'services/data/v43.0/query/';
-
 router.all('*', middleware.refreshToken);
-
-router.get('/events', async (req, res) => {
-  const query = 'SELECT+id,Subject +from+Event';
-  try {
-    const result = await request.salesforce(TEMP_INSTANCE_URL, TEMP_ROUTE, query, 'GET', {
-      Authorization: `Bearer ${TEMP_BEARER}`,
-    }, null, true);
-    res.status(200).json(result);
-  } catch (e) {
-    res.status(200).json(e.message);
-  }
-});
-
-router.get('/opportunities', async (req, res) => {
-  const query = 'SELECT+id,Name+from+Opportunity';
-  try {
-    const result = await request.salesforce(TEMP_INSTANCE_URL, TEMP_ROUTE, query, 'GET', {
-      Authorization: `Bearer ${TEMP_BEARER}`,
-    }, null, true);
-    res.status(200).json(result);
-  } catch (e) {
-    console.log('e.message :', e.message);
-    res.status(200).json(e.message);
-  }
-});
 
 router.post('/', async (req, res) => {
   const { integrationInfo, user, allIntegrations } = req.body;
