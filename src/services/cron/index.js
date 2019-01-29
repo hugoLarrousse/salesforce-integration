280bbc2cecc192b9006742bbf172a2ad7d629015
Hugo Larrousse <hugo.larrousse@gmail.com>
2019-01-29 17:53:20 +0100

    add try catch + remove useless element query

--- src/services/cron/index.js
+++ src/services/cron/index.js
@@ -33,30 +33,35 @@ const cronTask = async () => {
       throw new Error('No integrations');
     }
     for (const integration of allInfoForCron.integrations) {
-      const { user } = integration;
-      const integrationRefreshed = await refreshToken(integration);
-      const otherIntegrations = allInfoForCron.others.filter(other => String(other.orgaId) === String(integrationRefreshed.orgaId));
-      const date = new Date(Date.now() - (10 * MS_PER_MINUTE));
+      try {
+        const { user } = integration;
+        const integrationRefreshed = await refreshToken(integration);
+        const otherIntegrations = allInfoForCron.others.filter(other => String(other.orgaId) === String(integrationRefreshed.orgaId));
+        const date = new Date(Date.now() - (10 * MS_PER_MINUTE));
 
-      await syncData.syncByType(
-        integrationRefreshed, 'account', user, otherIntegrations,
-        'accountCron', `${date.toISOString().split('.')[0]}Z`, '/services/data/v43.0/queryAll/'
-      );
+        await syncData.syncByType(
+          integrationRefreshed, 'account', user, otherIntegrations,
+          'accountCron', `${date.toISOString().split('.')[0]}Z`, '/services/data/v43.0/queryAll/'
+        );
 
-      await syncData.syncByType(
-        integrationRefreshed, 'opportunity', user, otherIntegrations,
-        'opportunityCron', `${date.toISOString().split('.')[0]}Z`, '/services/data/v43.0/queryAll/'
-      );
+        await syncData.syncByType(
+          integrationRefreshed, 'opportunity', user, otherIntegrations,
+          'opportunityCron', `${date.toISOString().split('.')[0]}Z`, '/services/data/v43.0/queryAll/'
+        );
 
-      await syncData.syncByType(
-        integrationRefreshed, 'task', user, otherIntegrations,
-        'taskCron', `${date.toISOString().split('.')[0]}Z`, '/services/data/v43.0/queryAll/'
-      );
+        await syncData.syncByType(
+          integrationRefreshed, 'task', user, otherIntegrations,
+          'taskCron', `${date.toISOString().split('.')[0]}Z`, '/services/data/v43.0/queryAll/'
+        );
 
-      await syncData.syncByType(
-        integrationRefreshed, 'event', user, otherIntegrations,
-        'eventCron', `${date.toISOString().split('.')[0]}Z`, '/services/data/v43.0/queryAll/'
-      );
+        await syncData.syncByType(
+          integrationRefreshed, 'event', user, otherIntegrations,
+          'eventCron', `${date.toISOString().split('.')[0]}Z`, '/services/data/v43.0/queryAll/'
+        );
+      } catch (e) {
+        logger.error(__filename, 'for cronTask', e.message);
+        continue; // eslint-disable-line
+      }
     }
   } catch (e) {
     logger.error(__filename, 'cronTask', e.message);
